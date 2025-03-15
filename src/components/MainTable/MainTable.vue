209974e2ecc06a977322453102cb5d252e92beac
konbraphat51 <konbraphat@gmail.com>
2025-03-15 15:51:14 +0900

    clean up

---
+++ src/components/MainTable/MainTable.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="MainTable">
+		<tr>
+			fawe
+		</tr>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "MainTable",
+	props: {
+		peopleNum: Number,
+	},
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue"
+import { createI18n } from "vue-i18n"
+import App from "./App.vue"
 
-createApp(App).mount('#app')
+createApp(App).use(createI18n({})).mount("#app")
