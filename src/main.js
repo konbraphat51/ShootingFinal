44c6d58fdbdba4c7b73e5b326eb21fe05ed0a0fa
konbraphat51 <konbraphat@gmail.com>
2025-03-15 15:53:27 +0900

    i18n settings

--- src/main.js
+++ src/main.js
@@ -2,4 +2,22 @@ import { createApp } from "vue"
 import { createI18n } from "vue-i18n"
 import App from "./App.vue"
 
-createApp(App).use(createI18n({})).mount("#app")
+createApp(App)
+	.use(
+		createI18n({
+			fallbackLocale: "en",
+			messages: {
+				en: {
+					message: {
+						hello: "hello world",
+					},
+				},
+				ja: {
+					message: {
+						hello: "こんにちは、世界",
+					},
+				},
+			},
+		})
+	)
+	.mount("#app")
