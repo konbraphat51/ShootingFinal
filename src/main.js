888c2844124b0713488cc0baa882f9c38a62744c
konbraphat51 <konbraphat@gmail.com>
2025-03-15 15:53:55 +0900

    fix settings

--- src/main.js
+++ src/main.js
@@ -5,19 +5,7 @@ import App from "./App.vue"
 createApp(App)
 	.use(
 		createI18n({
-			fallbackLocale: "en",
-			messages: {
-				en: {
-					message: {
-						hello: "hello world",
-					},
-				},
-				ja: {
-					message: {
-						hello: "こんにちは、世界",
-					},
-				},
-			},
+			fallbackLocale: "ja",
 		})
 	)
 	.mount("#app")
