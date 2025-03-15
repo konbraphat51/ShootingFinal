306c3c19bc7c36c45c6d9900c58ec935ad5e46fc
konbraphat51 <konbraphat@gmail.com>
2025-03-15 16:15:52 +0900

    adding user

--- src/components/MainTable/MainTable.vue
+++ src/components/MainTable/MainTable.vue
@@ -1,7 +1,32 @@
 <template>
 	<div class="MainTable">
 		<tr>
-			fawe
+			<th>
+				{{ $t("MainTable.name") }}
+			</th>
+			<th>
+				{{ $t("MainTable.rank") }}
+			</th>
+			<th>
+				{{ $t("MainTable.totalScore") }}
+			</th>
+		</tr>
+		<tr v-for="user in users" :key="cnt">
+			<td>
+				{{ user.name }}
+			</td>
+			<td>
+				{{ user.rank }}
+			</td>
+			<td>
+				{{ user.totalScore }}
+			</td>
+		</tr>
+		<tr class="AddingRow">
+			<td>
+				<input type="text" v-model="newName" />
+			</td>
+			<td><button @click="addUser">Add</button></td>
 		</tr>
 	</div>
 </template>
@@ -9,8 +34,39 @@
 <script>
 export default {
 	name: "MainTable",
-	props: {
-		peopleNum: Number,
+	i18n: {
+		messages: {
+			en: {
+				MainTable: {
+					name: "Name",
+					rank: "Rank",
+					totalScore: "Total Score",
+				},
+			},
+			ja: {
+				MainTable: {
+					name: "名前",
+					rank: "順位",
+					totalScore: "合計",
+				},
+			},
+		},
+	},
+	data() {
+		return {
+			newName: "",
+			users: [],
+		}
+	},
+	methods: {
+		addUser() {
+			this.users.push({
+				name: this.newName,
+				rank: 0,
+				totalScore: 0,
+			})
+			this.newName = ""
+		},
 	},
 }
 </script>
