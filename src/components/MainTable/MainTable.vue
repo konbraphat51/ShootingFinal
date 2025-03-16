c481eb19e2c371d0419e2200fdb9cad2e49b640f
konbraphat51 <konbraphat@gmail.com>
2025-03-16 21:57:27 +0900

    update total scores and ranks

--- src/components/MainTable/MainTable.vue
+++ src/components/MainTable/MainTable.vue
@@ -70,8 +70,55 @@ export default {
 				name: this.newName,
 				rank: 0,
 				totalScore: 0,
+				scores: [],
 			})
 			this.newName = ""
+		},
+
+		_updateUsers() {
+			this._updateTotalScores()
+			this._updateRanks()
+		},
+
+		_updateTotalScores() {
+			this.users.forEach((user) => {
+				user.totalScore = 0
+				user.scores.forEach((score) => {
+					user.totalScore += score
+				})
+			})
+		},
+
+		_updateRanks() {
+			//sort total scores
+			//larger first
+			let totalScores = []
+			this.users.forEach((user) => {
+				totalScores.push(user.totalScore)
+			})
+			totalScores.sort((a, b) => b - a)
+
+			// create totalScore -> rank dictionary
+			let scoreToRank = {}
+			for (let cnt = 0; cnt < totalScores.length; cnt++) {
+				if (scoreToRank[totalScores[cnt]] === undefined) {
+					scoreToRank[totalScores[cnt]] = cnt + 1
+				}
+			}
+
+			// assign rank
+			this.users.forEach((user) => {
+				user.rank = scoreToRank[user.totalScore]
+			})
+		},
+	},
+
+	watch: {
+		users: {
+			handler() {
+				this._updateUsers()
+			},
+			deep: true,
 		},
 	},
 }
