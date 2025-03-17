7a5d40d233b45a84c9979601ff90307476bacf6d
konbraphat51 <konbraphat@gmail.com>
2025-03-17 23:27:53 +0900

    vertical lines

--- src/components/MainTable/MainTable.vue
+++ src/components/MainTable/MainTable.vue
@@ -180,6 +180,14 @@ table {
 	box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
 }
 
+th {
+	border-right: 1px solid #e0e0e0;
+}
+
+td {
+	border-right: 1px solid #e0e0e0;
+}
+
 th.Score {
 	background-color: #c4e7ff;
 }
