05c0de4b798f3a9bff19db2c0bec7c35c0cc0f27
konbraphat51 <konbraphat@gmail.com>
2025-05-01 22:53:34 +0900

    single HTML file

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import {defineConfig} from "vite"
+import {viteSingleFile} from "vite-plugin-singlefile"
 import vue from "@vitejs/plugin-vue"
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [vue(), viteSingleFile()],
 	base: "./",
 	build: {
 		outDir: "docs", // Github Pages
